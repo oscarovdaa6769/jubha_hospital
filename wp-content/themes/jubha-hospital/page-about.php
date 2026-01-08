50b2a416590fb5237af88164c13566921e9c1a97
bora4-netizen <chorkbora4@gmail.com>
2026-01-08 15:57:05 +0700

    hero find a doctor

---
+++ wp-content/themes/jubha-hospital/page-about.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<main>
+    <h1>hello</h1>
+</main>
+<?php get_footer(); ?>
---
+++ wp-content/themes/jubha-hospital/page-find-a-doctor.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<a href="<?php echo get_home_url(); ?> style.css">
+
+
+<div class="hero-find-doctor">
+  <div class="hero-content">
+    <h1>Find A Doctor</h1>
+    <button class="hero-btn">Find Now</button>
+  </div>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
